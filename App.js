9d944dcb9d80d0cf5752c22e612e3044e43167f8
Elizabeth Orellana <eorellana96@csu.fullerton.edu>
2024-09-25 17:11:23 -0700

    created the splash page. Still needs editing

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image,} from 'react-native';
+
+// const var for image
+const PlaceholderImage = require('./images/SmallWorld_Earth.png')
+const LeafPlaceholderImage= require('./images/leafO.png')
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Whats up!</Text>
+
+      <View style={styles.greenSection}>
+          <Text style={styles.headerText}>
+            Small W
+            <Image source={LeafPlaceholderImage} style={styles.leafimage}/>
+            rld</Text>
+
+      </View>
+
+      <View style={styles.blackSection}>
+       {/* image for home screen */}
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image}/>
+      </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    backgroundColor: '#76b947',
   },
+
+  greenSection:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+
+  headerText:{
+    color: '#FFF',
+    fontSize: 24,
+    fontWeight: 'bold',
+    flexDirection: 'row',
+    alignItems: "center",
+    textAlignVertical: "center",
+    fontFamily: "sans-serif"
+  },
+
+  leafimage:{
+    width: 20,
+    height: 20,
+    resizeMode: "contain",
+
+  },
+
+  blackSection: {
+    flex: 1,
+    backgroundColor: "black", 
+  },
+
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  image:{
+    flex: 1,
+    alignItems: "center",
+    resizeMode: "contain",
+    width: "100%",
+    height: "100%"
+  }
 });
