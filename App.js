7b526e0acc21bab44e39b49d2775979d976574f6
Nikolas Reyes <83786034+Nik-Reyes@users.noreply.github.com>
2024-10-03 17:29:32 -0700

    Added a dedicated expo splash screen. Would like the final version to add a fade-out/animatiomn on the tail end of the splash screen

--- App.js
+++ App.js
@@ -1,29 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image,} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useCallback, useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image, Animated } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
 
-// const var for image
-const PlaceholderImage = require('./images/SmallWorld_Earth.png')
-const LeafPlaceholderImage= require('./images/leafO.png')
-
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // This is where we will load the fonts and make any Necessary on-boot
+        // API calls. The fonts and API calls are loaded/made AND only then will
+        // the setTimeout begin
+        await new Promise((resolve) => setTimeout(resolve, 1500));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Once all on-boot loading is completed, make setAppIsReady to true
+        // so that the onLayoutRootView callback can hide the splash screen
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  // Hide the splash screen
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  // Has the same effect as the SplashScreen code above without all the
+  // error checking and checking whether things like fonts and APIs have
+  // loaded before actually loading the app
+
+  // useEffect(() => {
+  //   SplashScreen.preventAutoHideAsync();
+  //   setTimeout(async () => {
+  //     await SplashScreen.hideAsync();
+  //   }, 2000);
+  // }, []);
+
+  // App View
   return (
-    <View style={styles.container}>
-
-      <View style={styles.greenSection}>
-          <Text style={styles.headerText}>
-            Small W
-            <Image source={LeafPlaceholderImage} style={styles.leafimage}/>
-            rld</Text>
-
-      </View>
-
-      <View style={styles.blackSection}>
-       {/* image for home screen */}
-      <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image}/>
-      </View>
-      </View>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={styles.text}>This is your app. Customize away!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -33,48 +62,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    backgroundColor: '#76b947',
-  },
-
-  greenSection:{
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-  },
-
-  headerText:{
-    color: '#FFF',
-    fontSize: 24,
-    fontWeight: 'bold',
-    flexDirection: 'row',
+    backgroundColor: "#76b947",
+    justifyContent: "center",
     alignItems: "center",
-    textAlignVertical: "center",
-    fontFamily: "sans-serif"
   },
-
-  leafimage:{
-    width: 20,
-    height: 20,
-    resizeMode: "contain",
-
-  },
-
-  blackSection: {
-    flex: 1,
-    backgroundColor: "black", 
-  },
-
-  imageContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  image:{
-    flex: 1,
+  text: {
+    flexDirection: "row",
     alignItems: "center",
-    resizeMode: "contain",
-    width: "100%",
-    height: "100%"
-  }
+    justifyContent: "center",
+  },
 });
