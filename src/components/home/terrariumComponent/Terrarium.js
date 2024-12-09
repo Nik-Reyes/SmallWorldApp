ef5f31db60b0435685c4c595d260a88187d30568
Jordan Muir <jordanhmuir@gmail.com>
2024-12-08 22:02:39 -0800

    initialized git LFS with unity build files

--- src/components/home/terrariumComponent/Terrarium.js
+++ src/components/home/terrariumComponent/Terrarium.js
@@ -1,36 +1,50 @@
-import React, { useState, useRef } from "react";
-import { View, Text } from "react-native";
-import UnityView from "@azesmway/react-native-unity";
+import React, { useState, useRef, useEffect } from "react";
+import { View, Text, Platform, Alert } from "react-native";
+import UnityView from "@azesmway/react-native-unity";///lib/commonjs";
 import { dynamicContainerStyles } from "../../../hooks/buttonDimensions";
 import { styles } from "../../../styles/Home.styles";
 import Button from "../Button/Button";
 
 const terrariumImage = require("../../../../assets/images/garden.png");
 
+console.log("UnityView:", UnityView);
+if (!UnityView) {
+  console.warn("UnityView is null");
+}
+
 export default function Terrarium() {
   const [isUnityVisible, setUnityVisible] = useState(false);
-  const unityRef = useRef(null)
+  const [isUnitySupported, setUnitySupported] = useState(true);
+  const unityRef = useRef(null);
   const dynamicContainer = dynamicContainerStyles();
 
   const terrariumButtonPress = () => {
+    if (!isUnitySupported) {
+      Alert.alert(
+        "Unsupported Feature",
+        "Unity integration is not available on this device."
+      );
+      return;
+    }
     setUnityVisible(true);
     console.log("Terrarium Button Pressed");
-  }
+  };
 
   return (
     <View style={dynamicContainer("terrarium")}>
       <View style={styles.titleContainer}>
         <Text style={styles.titleText}>Your Terrarium</Text>
       </View>
-
       {isUnityVisible ? (
-        <UnityView
-          ref={unityRef}
-          style={{ flex: 1 }}
-          onUnityMessage={(result) => {
-            console.log("Unity Message Recieved:", result.nativeEvent.message);
-          }}
-        />
+        <View style={{ flex: 1 }}>
+            <UnityView
+              ref={unityRef}
+              style={{ flex: 1 }}
+              onUnityMessage={(result) => {
+                console.log("Unity Message Received:", result.nativeEvent.message);
+              }}
+            />
+        </View>
       ) : (
         <View style={styles.terrariumButtonContainer}>
           <Button
